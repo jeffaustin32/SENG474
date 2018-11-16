289795228f88f322d94a1d7c71b6218693d348c2
Jeff Austin <jeffaustin32@gmail.com>
2018-11-15 19:37:29 -0800

    Fix environment variable name

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const config = {
     ENV: process.env.NODE_ENV,
     PORT: process.env.PORT || 3200,
-    DATABASE_URL: process.env.DATABASE_URL || 'postgres://'
+    DATABASE_URL: process.env.HEROKU_POSTGRESQL_MAUVE_URL || 'postgres://'
 }
 
 module.exports = config;
