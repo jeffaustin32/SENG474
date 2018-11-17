12e922d3d6fdcba6cb6aef754325f2b0ef089720
Iain Emslie <iainemslie@gmail.com>
2018-11-16 17:06:57 -0800

    testing

--- query.js
+++ query.js
@@ -1,3 +1,3 @@
 module.exports = `
-    select * from county_facts natural join primary_results where candidate = 'Donald Trump';
+    select RHI825214, RHI225214 from county_facts natural join primary_results where candidate = 'Donald Trump' or candidate = 'Hillary Clinton';
 `
