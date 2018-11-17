673fb2adbd4e7e038ab28de8db4e97eb056636a9
Iain Emslie <iainemslie@gmail.com>
2018-11-16 21:59:28 -0800

    foreign_traveltime

--- query.js
+++ query.js
@@ -1,3 +1,3 @@
 module.exports = `
-    select VET605213, PVY020213, candidate  from primary_results natural join county_facts where candidate = 'Donald Trump' or candidate = 'Hillary Clinton';
+    select POP645213, LFE305213, candidate  from primary_results natural join county_facts where candidate = 'Donald Trump' or candidate = 'Hillary Clinton';
 `
