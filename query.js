ea68d625927f0c12afcae0810095106577cf5ce0
Iain Emslie <iainemslie@gmail.com>
2018-11-18 13:16:52 -0800

    blah

--- query.js
+++ query.js
@@ -1,3 +1,3 @@
 module.exports = `
-    select POP645213, LFE305213, candidate  from primary_results natural join county_facts where candidate = 'Donald Trump' or candidate = 'Hillary Clinton';
+    select clinton, trump, edu_batchelors, nonenglish from votes order by trump desc;
 `
