a9f707d37cc4858e7e5e79723b9088b6e6296ca1
Jeff Austin <jeffaustin32@gmail.com>
2018-11-18 15:59:37 -0800

    Updated query

--- query.js
+++ query.js
@@ -1,3 +1,13 @@
 module.exports = `
-    select clinton, trump, edu_batchelors, nonenglish from votes order by trump desc;
+    SELECT 
+        unemployment_view.avg as "avg_unemployment_2016",
+        votes.votes_dem_2016,
+        votes.votes_gop_2016,
+        votes.clinton,
+        votes.trump,
+        county_facts.*
+    FROM votes
+    LEFT JOIN unemployment_view ON votes.fips = unemployment_view.fips
+    LEFT JOIN county_facts ON votes.fips = county_facts.fips
+    ORDER BY fips ASC
 `
