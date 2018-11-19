0e2c2e6ff764b1a5802bb4819a306b966e56e9d6
Iain Emslie <iainemslie@gmail.com>
2018-11-18 16:57:05 -0800

    new

--- query.js
+++ query.js
@@ -1,13 +1,3 @@
 module.exports = `
-    SELECT 
-        unemployment_view.avg as "avg_unemployment_2016",
-        votes.votes_dem_2016,
-        votes.votes_gop_2016,
-        votes.clinton,
-        votes.trump,
-        county_facts.*
-    FROM votes
-    LEFT JOIN unemployment_view ON votes.fips = unemployment_view.fips
-    LEFT JOIN county_facts ON votes.fips = county_facts.fips
-    ORDER BY fips ASC
+    select clinton, trump, POP645213, poverty from votes order by trump desc;
 `
